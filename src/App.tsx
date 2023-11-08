fc70d32f4b8d67696820ea72e255fc73d238b971
pySatellite <pysatellite@gmail.com>
2023-11-08 14:22:50 +0900

    login

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,11 @@ import { PostList, PostEdit, PostCreate } from "./posts";
 import PostIcon from "@mui/icons-material/Book";
 import UserIcon from "@mui/icons-material/Group";
 import { Dashboard } from './Dashboard';
+import { authProvider } from './authProvider';
+
 
 export const App = () => (
-  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
+  <Admin dataProvider={dataProvider} dashboard={Dashboard} authProvider={authProvider} >
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
     <Resource name="users" list={UserList} show={ShowGuesser} recordRepresentation="name" icon={UserIcon}/>
   </Admin>
