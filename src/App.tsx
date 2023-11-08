1d177a8d6c74f24e442cbaa87c7761f647491bf4
pySatellite <pysatellite@gmail.com>
2023-11-08 13:40:36 +0900

    add button - edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import {
   Admin,
   Resource,
-  ListGuesser,
-  EditGuesser,
-  ShowGuesser,
 } from "react-admin";
 import { dataProvider } from "./dataProvider";
+import { UserList } from "./users";
+import { PostList } from "./posts";
 
 export const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={ListGuesser} />
+    <Resource name="posts" list={PostList} />
+    <Resource name="users" list={UserList} recordRepresentation="name" />
   </Admin>
 );
