f0707408059295559060978325582c7aeef1543a
pySatellite <pysatellite@gmail.com>
2023-11-08 12:41:29 +0900

    chage deploy target - https://satellite-info.web.app

--- src/App.tsx
+++ src/App.tsx
@@ -7,4 +7,8 @@ import {
 } from "react-admin";
 import { dataProvider } from "./dataProvider";
 
-export const App = () => <Admin dataProvider={dataProvider}></Admin>;
+export const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="users" list={ListGuesser} />
+  </Admin>
+);
