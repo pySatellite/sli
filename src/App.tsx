8846386e85a2bfc5345c06007d1f1bb25a3a56c1
pySatellite <87309910+pySatellite@users.noreply.github.com>
2023-11-09 15:18:03 +0900

    0.1.0/tutorial react admin (#1)
    * Let's learn
    
    * chage deploy target - https://satellite-info.web.app
    
    * add button - edit
    
    * Missing key prop for element in array
    
    * memu icon
    
    * dashbord
    
    * login
    
    * fix search

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import {
   Admin,
   Resource,
-  ListGuesser,
-  EditGuesser,
   ShowGuesser,
 } from "react-admin";
 import { dataProvider } from "./dataProvider";
+import { UserList } from "./users";
+import { PostList, PostEdit, PostCreate } from "./posts";
+import PostIcon from "@mui/icons-material/Book";
+import UserIcon from "@mui/icons-material/Group";
+import { Dashboard } from './Dashboard';
+import { authProvider } from './authProvider';
 
-export const App = () => <Admin dataProvider={dataProvider}></Admin>;
+
+export const App = () => (
+  <Admin dataProvider={dataProvider} dashboard={Dashboard} authProvider={authProvider} >
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
+    <Resource name="users" list={UserList} show={ShowGuesser} recordRepresentation="name" icon={UserIcon}/>
+  </Admin>
+);
