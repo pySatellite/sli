3c6768b51f20b1716965408c76529d84432331c6
pySatellite <pysatellite@gmail.com>
2023-11-09 15:10:00 +0900

    tutorial end

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,14 @@ import PostIcon from "@mui/icons-material/Book";
 import UserIcon from "@mui/icons-material/Group";
 import { Dashboard } from './Dashboard';
 import { authProvider } from './authProvider';
-import { PotatoeList } from './potatoes'
+import { PotatoeList } from './potatoes';
+import { OrionList } from './orions';
 
 
 export const App = () => (
   <Admin dataProvider={dataProvider} dashboard={Dashboard} authProvider={authProvider} >
     {/* <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/> */}
     {/* <Resource name="users" list={UserList} show={ShowGuesser} recordRepresentation="name" icon={UserIcon}/> */}
-    <Resource name="orions" list={ListGuesser} />
+    <Resource name="orions" list={OrionList} />
   </Admin>
 );
--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,8 +1,31 @@
-import { Card, CardContent, CardHeader } from "@mui/material";
+// import { Card, CardContent, CardHeader } from "@mui/material";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+const ver = import.meta.env.VITE_SLI_VERSION
 
 export const Dashboard = () => (
-    <Card>
-        <CardHeader title="Welcome to the administration" />
-        <CardContent>Lorem ipsum sic dolor amet...</CardContent>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="./images/satellite.jpeg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          SLI
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Satellite Location Information<br />
+          version : {ver}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
     </Card>
 );
